1a27dba7e208d3fbb57bafa9e3474d4237b31d1b
zunema <yxxa.abc@gmail.com>
2020-11-18 05:19:25 +0000

    [update]新規投稿のルーティング編集と部分テンプレートの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,15 @@
 class BooksController < ApplicationController
-  
+
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+
   end
 
   def index
@@ -12,9 +18,18 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+
+
   end
 
   def destroy
   end
-  
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
