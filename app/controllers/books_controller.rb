20d4b931a562d88e068a9a88dafeef73df2d4c91
zunema <yxxa.abc@gmail.com>
2020-11-20 07:13:32 +0000

    [update]編入ページからのupdateアクションでの詳細ページへのページ遷移編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,9 +18,16 @@ class BooksController < ApplicationController
     @user =  @book.user
     @newbook = Book.new
   end
-  
+
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+
   end
 
   def destroy
