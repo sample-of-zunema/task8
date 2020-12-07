6f75199bdcd874b5451d2c03edef5ae1ed47f11a
zunema <yxxa.abc@gmail.com>
2020-12-07 05:28:14 +0000

    [update]検索窓の条件分岐/完全一致・前方一致・後方一致・部分一致の実装

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,27 @@
 class SearchController < ApplicationController
+  
   def search
-    @users = User.where('name LIKE ?', "%#{params[:name]}%")
+    if params[:model] == "User"
+      if params[:search_method] == "perfect_match"
+        @users = User.where('name LIKE ?', "#{params[:name]}")
+      elsif params[:search_method] == "forward_match"
+        @users = User.where('name LIKE ?', "#{params[:name]}%")
+      elsif params[:search_method] == "backward_match"
+        @users = User.where('name LIKE ?', "%#{params[:name]}")
+      else
+        @users = User.where('name LIKE ?', "%#{params[:name]}%")
+      end
+    else
+      if params[:search_method] == "perfect_match"
+        @books = Book.where('title LIKE ?', "#{params[:name]}")
+      elsif params[:search_method] == "forward_match"
+        @books = Book.where('title LIKE ?', "#{params[:name]}%")
+      elsif params[:search_method] == "backward_match"
+        @books = Book.where('title LIKE ?', "%#{params[:name]}")
+      else
+        @books = Book.where('title LIKE ?', "%#{params[:name]}%")
+      end
+    end 
   end
+  
 end
